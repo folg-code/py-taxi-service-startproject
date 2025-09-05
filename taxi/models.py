247556ed5103bd23253034e6067ba00c4ee4358d
filip <filip1folga@gmail.com>
2025-09-05 11:55:13 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -25,7 +25,7 @@ class Car(models.Model):
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
     def __str__(self):
-        return f"{self.manufacturer.name} {self.model}"
+        return {self.manufacturer.name}
 
 
 
